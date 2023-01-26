9eede64afd5eaaee73b231de51586b7e71c26594
rhettb253 <rhettbeardemphl@hotmail.com>
2023-01-26 15:13:30 -0800

    got app done.. I think

--- js/catalog.js
+++ js/catalog.js
@@ -35,6 +35,7 @@ function handleSubmit(event) {
 // TODO: Add the selected item and quantity to the cart
 function addSelectedItemToCart() {
   // TODO: suss out the item picked from the select list
+  let selectedItem = 
   // TODO: get the quantity
   // TODO: using those, add one item to the Cart
 }
